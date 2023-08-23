54b1882f86c7c98c4f241b426aad6fe1a50eac1f
Simon NYSTRÖM <simon.nystroem@soprasteria.com>
2023-08-23 12:23:13 +0200

    added time formatting and author info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,43 @@
-import * as React from "react";
-import { Link, graphql } from "gatsby";
-import Layout from "../components/layout";
+import { Link, graphql } from "gatsby"
+import moment from "moment"
+import "moment/locale/sv"
+import * as React from "react"
+import Layout from "../components/layout"
 
 export default function Homepage({ data }) {
+  moment.locale("sv")
 
   const content = data.contentfulHomepage.content.map((content, i) => {
-      return (
-        <div key={i}>
-          <h2>{content.titel}</h2>
-          {content.article?.map((article, i) => {
-            return (
-              <div key={i}>
-                <Link to={article.path}> <h4>{article.titel}</h4></Link>
-                <div>{article.updatedAt}</div>
+    return (
+      <div key={i}>
+        <h2>{content.titel}</h2>
+        {content.article?.map((article, i) => {
+          return (
+            <div key={i}>
+              <Link to={article.path}>
+                {" "}
+                <h4>{article.titel}</h4>
+              </Link>
+              <div>
+                <div>
+                  <img
+                    src={
+                      article.reporter[0].profilePicture.gatsbyImageData.images
+                        .fallback.src
+                    }
+                  />
+                  <div>{article.reporter[0].name}</div>
+                </div>
+                <div>{moment(article.updatedAt).calendar()}</div>
               </div>
-            );
-          })}
-        </div>
-      )
-  });
+            </div>
+          )
+        })}
+      </div>
+    )
+  })
 
-  return <Layout>{content}</Layout>;
+  return <Layout>{content}</Layout>
 }
 
 export const query = graphql`
@@ -33,9 +50,15 @@ export const query = graphql`
             path
             titel
             updatedAt
+            reporter {
+              name
+              profilePicture {
+                gatsbyImageData
+              }
+            }
           }
         }
       }
     }
   }
-`;
+`
