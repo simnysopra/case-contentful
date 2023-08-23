bc6218264993a9b2b66653061e801641ceee3d74
Filip Sanderfelt <filip.sanderfelt@soprasteria.com>
2023-08-23 10:55:43 +0200

    Render rich text article

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,13 +1,26 @@
 import * as React from "react";
 import { graphql } from "gatsby"
 import Layout from "../components/layout";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default function Article({data}) {
-
+  const options = {
+    renderNode: {
+      'embedded-asset-block': node => (
+        <img
+          src={node.data.target.fields.file.url}
+          alt={node.data.target.fields.title || ''}
+        />
+      ),
+    },
+  };
   return <Layout>
     <h3>{data.contentfulArticle.titel}</h3>
     <div>{data.contentfulArticle.updatedAt}</div>
     <img src={data.contentfulArticle.omslagsBild.file.url} alt="articleImg" />
+    <div>
+                    {documentToReactComponents(JSON.parse(data.contentfulArticle.brodText.raw), options)}
+                </div>
   </Layout>;
 }
 
@@ -16,6 +29,9 @@ export const query = graphql`
   query ArticleContent($slug: String!) {
     contentfulArticle(path: { eq: $slug }) {
       titel
+      brodText{
+        raw
+      }
       kategori {
         titel
       }
